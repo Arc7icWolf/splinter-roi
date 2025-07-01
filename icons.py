5c7a1500c43424433722e5df1481b875d86c0ced
Arc7icWolf <162563901+Arc7icWolf@users.noreply.github.com>
2025-07-01 22:45:34 +0200

    Update icons.py

--- icons.py
+++ icons.py
@@ -1,21 +1,21 @@
 URL = "https://d36mxiodymuqjm.cloudfront.net/website/"
 
 color_icons = {
-  "Fire": f"{URL}collection/icon_element_fire.svg",
-  "Water": f"{URL}collection/icon_element_water_inactive.svg",
-  "Earth": f"{URL}collection/icon_element_earth_inactive.svg",
-  "Life": f"{URL}collection/icon_element_life_inactive.svg",
-  "Death": f"{URL}collection/icon_element_death_inactive.svg",
-  "Dragon": f"{URL}collection/icon_element_dragon_inactive.svg",
-  "Neutral": f"{URL}collection/icon_element_neutral_inactive.svg",
+  "Red": f"{URL}collection/icon_element_fire.svg",
+  "Blue": f"{URL}collection/icon_element_water_inactive.svg",
+  "Green": f"{URL}collection/icon_element_earth_inactive.svg",
+  "White": f"{URL}collection/icon_element_life_inactive.svg",
+  "Black": f"{URL}collection/icon_element_death_inactive.svg",
+  "Gold": f"{URL}collection/icon_element_dragon_inactive.svg",
+  "Gray": f"{URL}collection/icon_element_neutral_inactive.svg",
 }
 
 
 rarity_icons = {
-  "Common": f"{URL}create_team/icon_rarity_common_new.svg", 
-  "Rare": f"{URL}create_team/icon_rarity_rare_new.svg", 
-  "Epic": f"{URL}create_team/icon_rarity_epic_new.svg", 
-  "Legendary": f"{URL}create_team/icon_rarity_legendary_new.svg",
+  1: f"{URL}create_team/icon_rarity_common_new.svg", 
+  2: f"{URL}create_team/icon_rarity_rare_new.svg", 
+  3: f"{URL}create_team/icon_rarity_epic_new.svg", 
+  4: f"{URL}create_team/icon_rarity_legendary_new.svg",
 }
 
 
@@ -26,18 +26,18 @@ card_type_icons = {
 
 
 edition_icons = {
-    "Alpha Core": f"{URL}icons/icon-edition-alpha.svg",
-    "Promo": f"{URL}icons/icon-edition-promo.svg",
-    "Beta Core": f"{URL}icons/icon-edition-beta.svg",
-    "Beta, Untamed & Chaos Legion Reward": f"{URL}icons/icon-edition-reward.svg",
-    "Untamed Core": f"{URL}icons/icon-edition-untamed.svg",
-    "Azmare Dice": f"{URL}icons/icon-edition-dice.svg",
-    "Chaos Legion Core": f"{URL}icons/icon-edition-chaos.svg",
-    "Soulbound": f"{URL}icons/icon-edition-chaos.svg",
-    "Riftwatchers": f"{URL}icons/icon-edition-rift.svg",
-    "Rebellion Core": f"{URL}icons/icon-edition-rebellion.svg",
-    "Rebellion Reward": f"{URL}icons/icon-edition-rebellion.svg",
-    "Conclave Arcana": f"{URL}icons/icon-edition-conclave-arcana.svg",
+    0.1: f"{URL}icons/icon-edition-alpha.svg",
+    2: f"{URL}icons/icon-edition-promo.svg",
+    1: f"{URL}icons/icon-edition-beta.svg",
+    3: f"{URL}icons/icon-edition-reward.svg",
+    4: f"{URL}icons/icon-edition-untamed.svg",
+    5: f"{URL}icons/icon-edition-dice.svg",
+    7: f"{URL}icons/icon-edition-chaos.svg",
+    10: f"{URL}icons/icon-edition-chaos.svg",
+    8: f"{URL}icons/icon-edition-rift.svg",
+    12: f"{URL}icons/icon-edition-rebellion.svg",
+    13: f"{URL}icons/icon-edition-rebellion.svg",
+    14: f"{URL}icons/icon-edition-conclave-arcana.svg",
 }
 
 
