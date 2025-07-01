ad114b9ff64b3fad38fb3505a08a4e75950ad7d0
Arc7icWolf <162563901+Arc7icWolf@users.noreply.github.com>
2025-07-01 22:49:30 +0200

    Update icons.py

--- icons.py
+++ icons.py
@@ -26,18 +26,16 @@ card_type_icons = {
 
 
 edition_icons = {
-    0.1: f"{URL}icons/icon-edition-alpha.svg",
-    2: f"{URL}icons/icon-edition-promo.svg",
-    1: f"{URL}icons/icon-edition-beta.svg",
-    3: f"{URL}icons/icon-edition-reward.svg",
-    4: f"{URL}icons/icon-edition-untamed.svg",
-    5: f"{URL}icons/icon-edition-dice.svg",
-    7: f"{URL}icons/icon-edition-chaos.svg",
-    10: f"{URL}icons/icon-edition-chaos.svg",
-    8: f"{URL}icons/icon-edition-rift.svg",
-    12: f"{URL}icons/icon-edition-rebellion.svg",
-    13: f"{URL}icons/icon-edition-rebellion.svg",
-    14: f"{URL}icons/icon-edition-conclave-arcana.svg",
+    "0.1": f"{URL}icons/icon-edition-alpha.svg",
+    "2": f"{URL}icons/icon-edition-promo.svg",
+    "1": f"{URL}icons/icon-edition-beta.svg",
+    "3": f"{URL}icons/icon-edition-reward.svg",
+    "4": f"{URL}icons/icon-edition-untamed.svg",
+    "5": f"{URL}icons/icon-edition-dice.svg",
+    "7": f"{URL}icons/icon-edition-chaos.svg",
+    "10": f"{URL}icons/icon-edition-chaos.svg",
+    "8": f"{URL}icons/icon-edition-rift.svg",
+    "12": f"{URL}icons/icon-edition-rebellion.svg",
+    "13": f"{URL}icons/icon-edition-rebellion.svg",
+    "14": f"{URL}icons/icon-edition-conclave-arcana.svg",
 }
-
-
