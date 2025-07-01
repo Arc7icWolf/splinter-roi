223a517be84b0ad39655c57ff26e4aa0e6232632
Arc7icWolf <162563901+Arc7icWolf@users.noreply.github.com>
2025-07-01 19:19:36 +0200

    Update icons.py

--- icons.py
+++ icons.py
@@ -1,26 +1,43 @@
 URL = "https://d36mxiodymuqjm.cloudfront.net/website/"
 
-fire = f"{URL}collection/icon_element_fire.svg"
-water = f"{URL}collection/icon_element_water_inactive.svg"
-earth = f"{URL}collection/icon_element_earth_inactive.svg"
-life = f"{URL}collection/icon_element_life_inactive.svg"
-death = f"{URL}collection/icon_element_death_inactive.svg"
-dragon = f"{URL}collection/icon_element_dragon_inactive.svg"
-neutral = f"{URL}collection/icon_element_neutral_inactive.svg"
+color_icons = {
+  "Fire": f"{URL}collection/icon_element_fire.svg",
+  "Water": f"{URL}collection/icon_element_water_inactive.svg",
+  "Earth": f"{URL}collection/icon_element_earth_inactive.svg",
+  "Life": f"{URL}collection/icon_element_life_inactive.svg",
+  "Death": f"{URL}collection/icon_element_death_inactive.svg",
+  "Dragon": f"{URL}collection/icon_element_dragon_inactive.svg",
+  "Neutral": f"{URL}collection/icon_element_neutral_inactive.svg",
+}
 
-common = f"{URL}create_team/icon_rarity_common_new.svg"
-rare = f"{URL}create_team/icon_rarity_rare_new.svg"
-epic = f"{URL}create_team/icon_rarity_epic_new.svg"
-legendary = f"{URL}create_team/icon_rarity_legendary_new.svg"
 
-archons = f"{URL}collection/icon_filter_archons.svg"
-units = f"{URL}collection/icon_filter_units.svg"
+rarity_icons = {
+  "Common": f"{URL}create_team/icon_rarity_common_new.svg", 
+  "Rare": f"{URL}create_team/icon_rarity_rare_new.svg", 
+  "Epic": f"{URL}create_team/icon_rarity_epic_new.svg", 
+  "Legendary": f"{URL}create_team/icon_rarity_legendary_new.svg",
+}
 
-conclave_arcana = f"{URL}icons/icon-edition-conclave-arcana.svg"
-rebellion = f"{URL}icons/icon-edition-rebellion.svg"
-chaos = f"{URL}icons/icon-edition-chaos.svg"
-untamed = f"{URL}icons/icon-edition-untamed.svg"
-beta = f"{URL}icons/icon-edition-beta.svg"
-alpha = f"{URL}icons/icon-edition-alpha.svg"
-foundations = f"{URL}icons/icon-edition-foundations.svg"
-gladius = f"{URL}icons/icon-edition-gladius.svg"
+
+card_type_icons = {
+  "Summoner": f"{URL}collection/icon_filter_archons.svg", 
+  "Monster": f"{URL}collection/icon_filter_units.svg",
+}
+
+
+edition_icons = {
+    "Alpha Core": f"{URL}icons/icon-edition-alpha.svg",
+    "Promo": f"{URL}icons/icon-edition-promo.svg",
+    "Beta Core": f"{URL}icons/icon-edition-beta.svg",
+    "Beta, Untamed & Chaos Legion Reward": f"{URL}icons/icon-edition-reward.svg",
+    "Untamed Core": f"{URL}icons/icon-edition-untamed.svg",
+    "Azmare Dice": f"{URL}icons/icon-edition-dice.svg",
+    "Chaos Legion Core": f"{URL}icons/icon-edition-chaos.svg",
+    "Soulbound": f"{URL}icons/icon-edition-chaos.svg",
+    "Riftwatchers": f"{URL}icons/icon-edition-rift.svg",
+    "Rebellion Core": f"{URL}icons/icon-edition-rebellion.svg",
+    "Rebellion Reward": f"{URL}icons/icon-edition-rebellion.svg",
+    "Conclave Arcana": f"{URL}icons/icon-edition-conclave-arcana.svg",
+}
+
+
